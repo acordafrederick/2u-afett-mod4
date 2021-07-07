dd2d613f0c22dafe66da7e290cc75ecf01850cea
facorda <acorda.fred@gmail.com>
2021-07-07 11:21:11 -0400

    4.1.6 getRoomID error.

--- assets/js/room/index.js
+++ assets/js/room/index.js
@@ -1,1 +1,28 @@
 import "../../css/style.scss";
+
+const roomIdEl = document.querySelector("#room-id");
+
+let roomId = null;
+
+const getQueryStringParams = (query) => {
+  return query
+    ? (/^[?#]/.test(query) ? query.slice(1) : query)
+        .split("&")
+        .reduce((params, param) => {
+          let [key, value] = param.split("=");
+          params[key] = value
+            ? decodeURIComponent(value.replace(/\+/g, " "))
+            : "";
+          return params;
+        }, {})
+    : {};
+};
+
+const getRoomId = () => {
+  const params = getQueryStringParams(document.location.search);
+  roomId = params.roomId;
+  roomIdEl.textContent = roomId;
+  return params.roomId;
+};
+
+getRoomID();
