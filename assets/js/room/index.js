f9c256539e494372697b6a65eaa6c6a81fb191c9
facorda <acorda.fred@gmail.com>
2021-07-07 13:22:28 -0400

    updated room/index.js with Tamar Auber's code.

--- assets/js/room/index.js
+++ assets/js/room/index.js
@@ -1,9 +1,16 @@
 import "../../css/style.scss";
-
+// lesson 1
+const mainContentEl = document.querySelector("#main-content");
+const alertBoxEl = document.querySelector("#alert-box");
 const roomIdEl = document.querySelector("#room-id");
-
+const clipboardBtn = document.querySelector("#clipboard-btn");
+const localUserEl = document.querySelector("#local-username");
+const remoteUserEl = document.querySelector("#remote-username");
+const startCallBtnEl = document.querySelector("#start-call-btn");
+const stopCallBtnEl = document.querySelector("#stop-call-btn");
+// lesson 1
 let roomId = null;
-
+// lesson 1 (no refactor)
 const getQueryStringParams = (query) => {
   return query
     ? (/^[?#]/.test(query) ? query.slice(1) : query)
@@ -17,12 +24,26 @@ const getQueryStringParams = (query) => {
         }, {})
     : {};
 };
-
+// lesson 1 (no refactor)
 const getRoomId = () => {
   const params = getQueryStringParams(document.location.search);
+  // save roomId for reference
   roomId = params.roomId;
   roomIdEl.textContent = roomId;
   return params.roomId;
 };
-
-getRoomID();
+// lesson 1
+const copyToClipboard = async () => {
+  if (!navigator.clipboard) {
+    // Clipboard API not available
+    return;
+  }
+  try {
+    await navigator.clipboard.writeText(window.location.href);
+  } catch (err) {
+    console.error("Failed to copy!", err);
+  }
+};
+// lesson 1
+clipboardBtn.addEventListener("click", copyToClipboard);
+getRoomId();
