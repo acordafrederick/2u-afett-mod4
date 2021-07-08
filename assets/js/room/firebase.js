b3c9e73fd9970cb8e6553aaf41bf552c0a9bedc1
facorda <acorda.fred@gmail.com>
2021-07-08 19:26:05 -0400

    4.3.5 limit room to 2 users.

--- assets/js/room/firebase.js
+++ assets/js/room/firebase.js
@@ -4,6 +4,24 @@ let database = null;
 let userKey = null;
 let roomUsername = null;
 
+const checkRoomAvailability = async (database) => {
+  try {
+    const roomSnapshot = await database.once("value");
+    const roomOccupants = roomSnapshot.val();
+    if (!roomOccupants) {
+      return true;
+    }
+    if (Object.keys(roomOccupants).length < 2) {
+      return true;
+    }
+
+    return false;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
 export const joinRoom = async (roomId, username) => {
   try {
     // save username to `firebase.js` file for reference
@@ -11,6 +29,13 @@ export const joinRoom = async (roomId, username) => {
 
     // get reference to room
     database = firebase.database().ref(`/rooms/${roomId}`);
+
+    // check if room is full, set `isRoomOpen` to `false` if so
+    const isRoomOpen = await checkRoomAvailability(database);
+    // if room isn't open
+    if (!isRoomOpen) {
+      return false;
+    }
 
     // push user into room and create presence
     const user = await database.push({ username });
